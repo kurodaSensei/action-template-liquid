0f313eff1b58ed8d5994eddc04ff383bad24ff5c
Alfredo Romero <kuroda@KurodaSensei.local>
2023-12-22 16:41:50 -0500

    [test] trying to resolve an error with the github variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,8 +8,8 @@ fi
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-MODIFIED_FILES=$(git diff --name-only ${{ github.event.before }} ${{ github.sha }} | grep -E "\.(liquid|json)$")
+#MODIFIED_FILES=$(git diff --name-only ${{ github.event.before }} ${{ github.sha }} | grep -E "\.(liquid|json)$")
 
-echo "$MODIFIED_FILES" | xargs theme-check \
+echo xargs theme-check \
   | reviewdog -f=theme-check -name="theme-check" -reporter=github-pr-check
 
