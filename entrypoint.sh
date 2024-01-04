0042a8d4a04778c5b8c2106e7163ffa00230cdc9
Alfredo Romero <kuroda@KurodaSensei.local>
2024-01-04 13:19:42 -0500

    [test] changing the output and the format for reviewdog

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ fi
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-MODIFIED_FILES=$(git diff --name-only ${GITHUB_HEAD_REF} main | grep -E "\.(liquid|json)$")
+MODIFIED_FILES=$(git diff --name-only main | grep -E "\.(liquid|json)$")
 
 echo "$MODIFIED_FILES" | xargs theme-check -o json \
   | reviewdog -efm="%f:%l: %m,%-G%.%#" -name="theme-check" -reporter=github-pr-check
