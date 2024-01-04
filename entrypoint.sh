efc578f02fcd106ae271813bd86670a96b5c3326
Alfredo Romero <kuroda@KurodaSensei.local>
2024-01-04 17:57:44 -0500

    [test] changing the output and the format for reviewdog

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,4 +8,5 @@ fi
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-theme-check -o json . | reviewdog -efm="%f:%l: %m,%-G%.%#" -name="theme-check" -reporter=github-pr-check
+theme-check -o json . 
+#| reviewdog -efm="%f:%l: %m,%-G%.%#" -name="theme-check" -reporter=github-pr-check
